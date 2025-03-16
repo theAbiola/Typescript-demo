2e412a57b0938e3f36a48fc1eee787b6e2b0d5c7
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-16 15:42:43 +0100

    use Type Alises

--- 0.2 Basics/myObjects.ts
+++ 0.2 Basics/myObjects.ts
@@ -1,10 +1,22 @@
-function createUser({ name: string, isPaid: boolean }) {
+function createUser({ name: string, isPaid: boolean }) { //what the function should accept
 
 }
 createUser({ name: 'Habib', isPaid: true });
 
-function createCourse(): { name: string, price: number } {
+function createCourse(): { name: string, price: number } { //what the function should return
     return { name: 'ReactJS', price: 100 };
 }
+
+//TYPE ALIAS
+type User = { //creating our own type which is an object with the datatypes that we set inside it(TYPE ALIAS)
+    name: string;
+    email: string;
+    isPaid: boolean;
+}
+
+function createUser2(user: User) {
+    console.log(user);
+}
+createUser2({ name: 'Habib', email: 'myEmail@gmail.com', isPaid: true });
 
 export { }
