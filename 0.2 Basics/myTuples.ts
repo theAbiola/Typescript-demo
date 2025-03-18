3155f38d13f6af6b6478533cad50462d33b21793
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-18 12:45:31 +0100

    start with tuples

---
+++ 0.2 Basics/myTuples.ts
@@ -0,0 +1,7 @@
+//tuples mean that we can define an array with a fixed number of elements and types
+//Being fixed means that the input data must follow the order of the types and the number of elements in the tuple
+//For example, if we define a tuple as [string, number, boolean], the input data must follow this order and have exactly 3 elements
+//The three elements must be a string, a number and a boolean in that order
+
+
+let myData: [string, number, boolean] = ['Habib', 1, true]; //This is a tuple
