dce8928fc047ad7bafcad3839bf5991a7605e562
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-18 13:16:21 +0100

    transpile myTuples.ts

--- 0.2 Basics/myTuples.js
+++ 0.2 Basics/myTuples.js
@@ -2,18 +2,10 @@
 //Being fixed means that the input data must follow the order of the types and the number of elements in the tuple
 //For example, if we define a tuple as [string, number, boolean], the input data must follow this order and have exactly 3 elements
 //The three elements must be a string, a number and a boolean in that order
-
-
-let myData: [string, number, boolean] = ['Habib', 1, true]; //This is a tuple
-
-let rgb: [number, number, number] = [255, 125, 150]; //This is a tuple
-
-type Person = [number, string, string]; //This is a type alias for a tuple
-
-let person1: Person = [1, 'Habib', 'myemail@mail.com']; //This is a tuple
-
+var myData = ['Habib', 1, true]; //This is a tuple
+var rgb = [255, 125, 150]; //This is a tuple
+var person1 = [1, 'Habib', 'myemail@mail.com']; //This is a tuple
 person1[1] = 'Habibat'; //This is allowed
-
 //tuples have an issue with the push method because it can change the order of the elements in the tuple
-/*Example:*/ person1.push(40) //This shouldn't be allowed bcus it would mean that we are changing the order of the elements in the tuple that we set in the 'Person' type alias
-console.log(person1); 
+/*Example:*/ person1.push(40);
+console.log(person1); //This is allowed even though we are changing the order of the elements in the tuple 
