8398e57cc8291168cb1b0fec4184909a17408aaa
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-19 14:10:24 +0100

    add classes

--- 0.3 PureTS/dist/index.js
+++ 0.3 PureTS/dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('Hello from Typescript!');
+class User {
+    constructor(name, email) {
+        this.city = ''; //private means we can only access this property inside the User class declaration
+        this.name = name;
+        this.email = email;
+        this.city;
+    }
+}
+class User2 {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.city = ''; //private means we can only access this property inside the User class declaration
+    }
+}
+let firstUser = new User('Habib', 'h@a.com');
+let secondUser = new User2('Biola', 'a@b.com');
+//user1.city = 'Abuja' //this will throw an error because we set city property to readonly on line 4 above
--- 0.3 PureTS/src/index.ts
+++ 0.3 PureTS/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello from Typescript!')
+class User {
+    name: string;
+    email: string;
+    private readonly city: string = '' //private means we can only access this property inside the User class declaration
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+        this.city
+    }
+}
+
+class User2 {
+    private readonly city: string = '' //private means we can only access this property inside the User class declaration
+    constructor(
+        public name: string,
+        public email: string,
+        //private city: string
+    ) {
+    }
+}
+
+let firstUser = new User('Habib', 'h@a.com');
+let secondUser = new User2('Biola', 'a@b.com')
+//user1.city = 'Abuja' //this will throw an error because we set city property to readonly on line 4 above
